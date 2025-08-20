f7292e85f848b54dbe472621db971ac5ea38451a
Mario Martinez Mejia <mariomejiamartinez208@gmail.com>
2025-08-20 16:02:59 -0600

    Activación de la configuración automática de Spring Boot con @EnableJpaRepositories

--- src/main/java/com/spring/ecomers/EComersApplication.java
+++ src/main/java/com/spring/ecomers/EComersApplication.java
@@ -2,10 +2,12 @@ package com.spring.ecomers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.spring.ecomers.repository")
+//No se para qye es esto
+@EnableJpaRepositories(basePackages = "com.spring.ecomers.repository") /// Activa la configuración automática de Spring Boot
 public class EComersApplication {
 
     public static void main(String[] args) {
